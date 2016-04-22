cf2547ed485882675a027400fa1e8909b8c7a664
elizabetaaleksieva <elizabeta.aleksieva@gmail.com>
2016-04-22 19:03:46 +0300

    Signed-off-by: elizabetaaleksieva <elizabeta.aleksieva@gmail.com>

---
+++ [Swift][HW PHP - Introduction, Conditional Statement, Functions, Arrays/[SWIFT][HW ContidionalStatements 7.2]/8.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+        /* Напишете програма, която приема променлива от 
+        тип Число (в размера0, – и разпечатва 
+        тяхната стойност от тип стринг */
+        
+      $T2 = "two";
+      $O1 = "one";
+      $O0 = "zero";
+      $hi = "not a digit";
+      $T10 = "ten";
+     
+    
+      echo $T2;
+      echo '<br />';
+      echo $O1;
+      echo '<br />';
+      echo $O0;
+      echo '<br />';
+      echo $hi;
+      echo '<br />';
+      echo $T10;
+      echo '<br />';
+      
+      
+      
+      
+        
+        
+        
+        
+        ?>
+    </body>
+</html>
